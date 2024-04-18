749532f8cdbdbecbcf2d271b6819abe107d0e14f
Raykrad <109039576+Raykrad@users.noreply.github.com>
2024-04-18 19:22:05 +0300

    Create install_windows.bat

---
+++ external/install_windows.bat
@@ -0,0 +1,10 @@
+git clone https://github.com/g-truc/glm.git
+echo 'GLM'
+
+git clone https://github.com/glfw/glfw.git
+echo 'GLFW'
+
+git clone https://github.com/ocornut/imgui.git
+echo 'ImGui'
+
+echo 'Installation all complide'
